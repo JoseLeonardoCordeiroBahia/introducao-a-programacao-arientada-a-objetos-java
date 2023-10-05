affdc74a135059bd698a2dd1bb15140758710f31
José Leonardo <leonardo_cordeirobahia@hotmail.com>
2023-10-04 21:09:34 -0300

    Finalizando o programa

--- src/exemplo/exemplo2/application/Program.java
+++ src/exemplo/exemplo2/application/Program.java
@@ -25,7 +25,24 @@ public class Program {
 		System.out.print("Quantity in stock: ");
 		product.quantity = scanner.nextInt();
 		
-		System.out.println(product);
+		System.out.println();
+		System.out.println("Product data: " + product);
+		
+		System.out.println();
+		System.out.print("Enter the number of products to be added in stock: ");
+		int quantity = scanner.nextInt();
+		product.addProducts(quantity);
+		
+		System.out.println();
+		System.out.println("Updated data: " + product);
+		
+		System.out.println();
+		System.out.print("Enter the number of products to be removed in stock: ");
+		quantity = scanner.nextInt();
+		product.removeProducts(quantity);
+		
+		System.out.println();
+		System.out.println("Updated data: " + product);
 		
 		scanner.close();
 	}
